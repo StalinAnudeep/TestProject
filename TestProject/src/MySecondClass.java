efadec9a12854e9add2aa016c4187bc6d9eb77a4
stalin <stalin@esocietybil.com>
2018-10-06 16:16:18 +0530

    Add New Java Class

--- TestProject/src/MySecondClass.java
+++ TestProject/src/MySecondClass.java
@@ -1,10 +1,9 @@
 
-public class MyFirstClass {
+public class MySecondClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-		
+		System.out.println("My Second Class");
 	}
 
 }
